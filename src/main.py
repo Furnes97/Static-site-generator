8efcb043e3d0a09a08a18c812859f325bdc46a4b
Furnes97 <furnes.birger@gmail.com>
2025-05-29 22:34:28 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+def main():
+    text_1 = TextNode("This is some anchor text", TextType.ITALIC, "https://www.boot.dev")
+    print(text_1)
+
+
+
+
+main()
