784d75541031e814d829e30769c0b4250398c85b
Furnes97 <furnes.birger@gmail.com>
2025-05-30 02:08:19 +0200

    finished parent node, recursion

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -49,7 +49,47 @@ class LeafNode(HTMLNode):
 
 
 
-        #dictionary_tags = {
-        #"a":["<a>", "<a/>"],
-        #"p":["<a","</a>"]
-        #}
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children ,props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag is required for parentnode")
+        if self.children is None:
+            raise ValueError("Children are missing")
+
+        return f'<{self.tag}>{"".join(list(map(lambda childnode: childnode.to_html(),self.children)))}</{self.tag}>'
+
+"""
+def text_node_to_html_node(text_node):
+    if text_node != TextType.TEXT.value:
+        return
+
+    elif TextType.BOLD:
+    elif TextType.ITALIC:
+    elif TextType.CODE:
+    elif TextType.LINK:
+    elif TextType.IMAGE:
+    else:
+        raise Exception("Wrong node")
+
+
+"""
+
+"""
+t should handle each type of the TextType enum. If it gets a TextNode that is none of those types, it should raise an exception. Otherwise, it should return a new LeafNode object.
+
+TextType.TEXT: This should return a LeafNode with no tag, just a raw text value.
+TextType.BOLD: This should return a LeafNode with a "b" tag and the text
+TextType.ITALIC: "i" tag, text
+TextType.CODE: "code" tag, text
+TextType.LINK: "a" tag, anchor text, and "href" prop
+TextType.IMAGE: "img" tag, empty string value, "src" and "alt" props ("src" is the image URL, "alt" is the alt text)
+"""
+
+
+"""
+def __init__(self, tag, value, props=None):
+    super().__init__(tag, value, None ,props)
+"""
